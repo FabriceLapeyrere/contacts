eb0dcd5301fece15d7906c79b0c7b6645451b497
Fabrice Lapeyrere <fabrice.lapeyrere@surlefil.org>
2019-04-03 16:20:59 +0200

    correctif dans la redirection de lien

--- r.php
+++ r.php
@@ -11,53 +11,55 @@ $params=json_decode(base64_decode($hash),true);
 $contact=$params['contact'];
 $envoi=$params['envoi'];
 $url=$params['url'];
-$isimg=isset($params['isImg']) ? $params['isImg'] : 0;
-if ($url=="") {
-	exit();
-}
-if ($isimg==1){
-	// on redirige
-	if (file_exists($url)) {
-		while(ob_get_level()) ob_end_clean();
-		header('Connection: close');
-		ignore_user_abort();
-		ob_start();
-		$path_parts=pathinfo($url);
-		switch(strtolower($path_parts['extension'])) {
-			case "gif":
-			header("Content-type: image/gif");
-			break;
-			case "jpg":
-			case "jpeg":
-			header("Content-type: image/jpeg");
-			break;
-			case "png":
-			header("Content-type: image/png");
-			break;
-			case "bmp":
-			header("Content-type: image/bmp");
-			break;
-		}
-		header('Content-Length: ' . filesize($url));
-		header('Content-Disposition: filename=' . basename($url));
-		readfile($url);
-		ob_end_flush();
-		flush();
-	} else {
-		exit();
-	}
-} else {
-	// on redirige
-	while(ob_get_level()) ob_end_clean();
-	header('Connection: close');
-	ignore_user_abort();
-	ob_start();
-	$size = ob_get_length();
-	header("Content-Length: $size");
-	header('location:'.$url);
-	ob_end_flush();
-	flush();
-}
+if (ctype_digit(strval($contact)) && ctype_digit(strval($envoi))) {
+    // $contact is an integer
+    $isimg=isset($params['isImg']) ? $params['isImg'] : 0;
+    if ($url=="") {
+    	exit();
+    }
+    if ($isimg==1){
+    	// on redirige
+    	if (file_exists($url)) {
+    		while(ob_get_level()) ob_end_clean();
+    		header('Connection: close');
+    		ignore_user_abort();
+    		ob_start();
+    		$path_parts=pathinfo($url);
+    		switch(strtolower($path_parts['extension'])) {
+    			case "gif":
+    			header("Content-type: image/gif");
+    			break;
+    			case "jpg":
+    			case "jpeg":
+    			header("Content-type: image/jpeg");
+    			break;
+    			case "png":
+    			header("Content-type: image/png");
+    			break;
+    			case "bmp":
+    			header("Content-type: image/bmp");
+    			break;
+    		}
+    		header('Content-Length: ' . filesize($url));
+    		header('Content-Disposition: filename=' . basename($url));
+    		readfile($url);
+    		ob_end_flush();
+    		flush();
+    	} else {
+    		exit();
+    	}
+    } else {
+    	// on redirige
+    	while(ob_get_level()) ob_end_clean();
+    	header('Connection: close');
+    	ignore_user_abort();
+    	ob_start();
+    	$size = ob_get_length();
+    	header("Content-Length: $size");
+    	header('location:'.$url);
+    	ob_end_flush();
+    	flush();
+    }
 	// on remplit la base
 	$db= new DB(true);
 	$c=Contacts::get_casquette($contact,false,1);
@@ -76,7 +78,7 @@ if ($isimg==1){
 			$insert->execute(array($contact, $envoi, 'Lu', $date));
 			$notify=true;
 		}
-		$db->database->commit();		
+		$db->database->commit();
 	} else {
 		$insert = $db->database->prepare('INSERT INTO r (id_cas, id_envoi, url, date) VALUES (?,?,?,?) ');
 		$insert->execute(array($contact, $envoi, $url, $date));
@@ -89,7 +91,7 @@ if ($isimg==1){
 			$e=Mailing::get_envoi($envoi,'',1);
 			$expediteur=$e['expediteur'];
 			$exp=$C->mailing->expediteurs->value[$expediteur->id];
-	
+
 			require 'server/lib/PHPMailer/PHPMailerAutoload.php';
 			$mail = new PHPMailer();
 			$mail->SetLanguage("fr","server/lib/PHPmailer/language/");
@@ -108,4 +110,27 @@ if ($isimg==1){
 			$mail->Send();
 		}
 	}
-
+} else {
+    if (isset($params['url'])) {
+        // on redirige
+    	while(ob_get_level()) ob_end_clean();
+    	header('Connection: close');
+    	ignore_user_abort();
+    	ob_start();
+    	$size = ob_get_length();
+    	header("Content-Length: $size");
+    	header('location:'.$url);
+    	ob_end_flush();
+    	flush();
+    } else {
+?>
+<head>
+    <title>Lien malformé...</title>
+    <meta charset="UTF-8">
+</head>
+<body>
+    Le lien est malformé, désolé...
+</body>
+<?
+    }
+}
